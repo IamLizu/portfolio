58fe50b5aa07900e19cf8c67261ee645e789e6ad
IamLizu <thegeek@iamlizu.com>
2020-08-25 15:12:19 +0600

    migrated info to json file instead of hard coding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import PersonalInfo from '../portfolio-data/personal.json'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,11 +31,11 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h4" className={classes.title}>
-            Portfolio <span className={classes.navName}>of <a className={classes.navLink} href="https://iamlizu.com/">S M Mahmudul Hasan</a></span>
+            Portfolio <span className={classes.navName}>of <a className={classes.navLink} href="https://iamlizu.com/">{PersonalInfo.name}</a></span>
           </Typography>
-          <Button color="inherit"> <a className={classes.navLink} href="https://iamlizu.com/blog/">Blog</a> </Button>
-          <Button color="inherit"> <a className={classes.navLink} href="https://github.com/IamLizu">GitHub</a> </Button>
-          <Button color="inherit"> <a className={classes.navLink} href="https://twitter.com/IamLizu">Twitter</a> </Button>
+          <Button color="inherit"> <a className={classes.navLink} target="_blank" rel="noopener noreferrer" href={PersonalInfo.blog}>Blog</a> </Button>
+          <Button color="inherit"> <a className={classes.navLink} target="_blank" rel="noopener noreferrer" href={`https://github.com/${PersonalInfo.github}`}>GitHub</a> </Button>
+          <Button color="inherit"> <a className={classes.navLink} target="_blank" rel="noopener noreferrer" href={`https://twitter.com/${PersonalInfo.twitter}`}>Twitter</a> </Button>
         </Toolbar>
       </AppBar>
     </div>
